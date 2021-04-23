debf5545efab053fed0c2f5ad040be5513bea2de
Eva <Eva.Moulard@laposte.net>
2021-04-23 14:44:28 +0200

    ....

--- teste.py
+++ teste.py
@@ -1,8 +1,8 @@
 import pandas
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.datasets import make_regression
-from sklearn.linear_model import SGDRegressor
+from sklearn.model_selection import train_test_split
+from sklearn import linear_model
 
 # Récupération des données
 # bostonHousePrices = pandas.read_csv('boston_house_prices.csv')
@@ -12,12 +12,23 @@ regSimple = pandas.read_csv('reg_simple.csv')
 
 # Visualisation des données
 print(regSimple)
-x = regSimple['heure_rev']
-y = regSimple['note']
-plt.scatter(regSimple['heure_rev'], regSimple['note'])
 
-X = np.hstack(x, np.ones(x.shape))
-X.shape
+
+# np.random.seed(0)
+
+# n_samples = 27
+# x = np.linspace(0, 100, n_samples).reshape(n_samples, 1)
+# y = x + np.random.randn(n_samples, 1)
+
+# plt.scatter(x, y)
+# plt.show()
+
+# X = np.hstack((x, np.ones(x.shape)))
+# print(X.shape)
+
+# theta = np.random.randn(2, 1)
+# print(theta)
+
 # def model(X, theta):
 #     return X.dot(theta)
 
@@ -27,22 +38,34 @@ X.shape
 
 # def grad(X, y, theta):
 #     n = len(y)
-#     return 1/n * X.T.dot(model(X, theta) - y)
+#     return 1/m * X.T.dot(model(X, theta) - y)
 
 # def gradient_descent(x, y, theta, learning_rate, n_iterations):
 #     cost_history = np.zeros(n_iterations)
+
 #     for i in range(0, n_iterations):
 #         theta = theta - learning_rate * grad(x, y, theta)
-        
 #         cost_history[i] = cost_function(x, y, theta)
 
-#     print(theta.shape)
 #     return theta, cost_history
 
-# def coef_determination(y, pred):
-#     u = ((y - pred)**2).sum()
-#     v = ((y - y.mean())**2).sum()
-#     return 1 - u/v
+# # Example de test :
+# print(cost_function(X, y, theta)) # pas d'erreur, retourne float, ~ 1000
+
+# n_iterations = 1000
+# learning_rate = 0.01
+ 
+# theta_final, cost_history = gradient_descent(X, y, theta, learning_rate, n_iterations)
+ 
+# print(theta_final) # voici les parametres du modele une fois que la machine a été entrainée
+ 
+# # création d'un vecteur prédictions qui contient les prédictions de notre modele final
+# predictions = model(X, theta_final)
+ 
+# # Affiche les résultats de prédictions (en rouge) par rapport a notre Dataset (en bleu)
+# plt.scatter(x, y)
+# plt.plot(x, predictions, c='r')
+# plt.show()
 
 
 # x = np.array(df['heure_rev'])
