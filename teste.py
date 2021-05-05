66483c7a792577e4ac744ebb197f1d196a1e71c8
RexiaEva <Eva.Moulard@laposte.net>
2021-05-05 17:13:54 +0200

    ...

--- teste.py
+++ teste.py
@@ -1,99 +1,32 @@
-import pandas
-import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
-from sklearn.model_selection import train_test_split
-from sklearn import linear_model
+from scipy import stats
 
 # Récupération des données
 # bostonHousePrices = pandas.read_csv('boston_house_prices.csv')
 # positionSalaries = pandas.read_csv('Position_Salaries.csv')
 # qualiteVinRouge = pandas.read_csv('qualite_vin_rouge.csv')
-regSimple = pandas.read_csv('reg_simple.csv')
+regSimple = pd.read_csv('reg_simple.csv')
 
 # Visualisation des données
-print(regSimple)
+#selection de la première colonne de notre dataset (la taille de la population)
+X = regSimple.iloc[0:len(regSimple),0]
+#selection de deuxième colonnes de notre dataset (le profit effectué)
+Y = regSimple.iloc[0:len(regSimple),1] 
+axes = plt.axes()
+axes.grid() # dessiner une grille pour une meilleur lisibilité du graphe
+plt.scatter(X,Y) # X et Y sont les variables qu'on a extraite dans le paragraphe précédent
+plt.show()
 
+# Création du modèle (model(X,theta))
+#linregress() renvoie plusieurs variables de retour. On s'interessera 
+# particulierement au slope et intercept
+slope, intercept, r_value, p_value, std_err = stats.linregress(X, Y)
 
-# np.random.seed(0)
+def predict(x):
+   return slope * x + intercept
 
-# n_samples = 27
-# x = np.linspace(0, 100, n_samples).reshape(n_samples, 1)
-# y = x + np.random.randn(n_samples, 1)
-
-# plt.scatter(x, y)
-# plt.show()
-
-# X = np.hstack((x, np.ones(x.shape)))
-# print(X.shape)
-
-# theta = np.random.randn(2, 1)
-# print(theta)
-
-# def model(X, theta):
-#     return X.dot(theta)
-
-# def cost_function(X, y, theta):
-#     n = len(y)
-#     return 1/(2*n) * np.sum((model(X, theta) - y)**2)
-
-# def grad(X, y, theta):
-#     n = len(y)
-#     return 1/m * X.T.dot(model(X, theta) - y)
-
-# def gradient_descent(x, y, theta, learning_rate, n_iterations):
-#     cost_history = np.zeros(n_iterations)
-
-#     for i in range(0, n_iterations):
-#         theta = theta - learning_rate * grad(x, y, theta)
-#         cost_history[i] = cost_function(x, y, theta)
-
-#     return theta, cost_history
-
-# # Example de test :
-# print(cost_function(X, y, theta)) # pas d'erreur, retourne float, ~ 1000
-
-# n_iterations = 1000
-# learning_rate = 0.01
- 
-# theta_final, cost_history = gradient_descent(X, y, theta, learning_rate, n_iterations)
- 
-# print(theta_final) # voici les parametres du modele une fois que la machine a été entrainée
- 
-# # création d'un vecteur prédictions qui contient les prédictions de notre modele final
-# predictions = model(X, theta_final)
- 
-# # Affiche les résultats de prédictions (en rouge) par rapport a notre Dataset (en bleu)
-# plt.scatter(x, y)
-# plt.plot(x, predictions, c='r')
-# plt.show()
-
-
-# x = np.array(df['heure_rev'])
-# y = np.array(df['note'])
-# plt.scatter(x,y)
-# plt.show()
-# print(x.shape)
-# x = x.reshape(x.shape[0], 1)
-# y = y.reshape(y.shape[0], 1)
-# print(y.shape)
-# print("=============================================")
-# x = np.hstack((x, np.ones(x.shape)))
-# print(x)
-# print("=============================================")
-# theta = np.random.randn(2, 1)
-# print(theta)
-# print("=============================================")
-# plt.scatter(x[:,0],y)
-# plt.plot(x[:,0], model(x,theta), c='r')
-# plt.show()
-# print("=============================================")
-# # print(cost_function(x, y, theta))
-# theta_final , cost_history= gradient_descent(x, y, theta, 0.001, 1000)
-# print(theta_final)
-# predictions = model(x, theta_final)
-# plt.scatter(x[:,0], y)
-# plt.plot(x[:,0], predictions, c='r')
-# plt.show()
-# plt.plot(range(1000), cost_history)
-# plt.show()
-# print(coef_determination(y, predictions))
+#la variable fitLine sera un tableau de valeurs prédites depuis la tableau de variables X
+fitLine = predict(X)
+plt.plot(X, fitLine, c='r')
+plt.show()
